a42b0a2eb45d73641595cb1cb28d94372850ac68
bh <bahar.brt2004@gmail.com>
2025-09-17 15:20:46 +0330

    add

--- ProjectPopup/Assets/Scriptes/PigSoulWalk.cs
+++ ProjectPopup/Assets/Scriptes/PigSoulWalk.cs
@@ -1,16 +1,58 @@
 using UnityEngine;
 
-public class PigSoulWalk : MonoBehaviour
+[RequireComponent(typeof(Rigidbody2D))]
+public class SimpleWalker : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [SerializeField] private float moveSpeed = 5f;
+
+    private Rigidbody2D rb;
+    private Animator anim;
+
+    private bool walking;
+    private bool facingRight = true;
+
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
+        float h = Input.GetAxisRaw("Horizontal"); 
+        bool hasInput = Mathf.Abs(h) > 0.01f;
         
+        if (hasInput && !walking)
+        {
+            walking = true;
+            anim.ResetTrigger("StopWalk");
+            anim.SetTrigger("StartWalk");
+        }
+        
+        if (!hasInput && walking)
+        {
+            walking = false;
+            anim.ResetTrigger("StartWalk");
+            anim.SetTrigger("StopWalk");
+        }
+        
+        if (h > 0 && !facingRight)
+            Flip();
+        else if (h < 0 && facingRight)
+            Flip();
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2(h * moveSpeed, 0f);
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 scale = transform.localScale;
+        scale.x *= -1;
+        transform.localScale = scale;
     }
 }
